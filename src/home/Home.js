cdd88eb0babcd13056b52bbbb50e7762eea1ffa3
Katherine Tam <katherinemtam@gmail.com>
2021-05-20 10:28:31 -0700

    change homepage

--- src/home/Home.js
+++ src/home/Home.js
@@ -7,7 +7,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h2>Home Page</h2>
+        <h2>Welcome to the Met Museum Artworks Gallery!</h2>
 
         <Link to='/resources'>See the List</Link>
       </div>
