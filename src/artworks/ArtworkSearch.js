6d2c89dfc17fdc288d9543ee4a4a991008717bcf
cmmerritt <christiane.merritt@gmail.com>
2021-05-20 11:27:47 -0700

    start artwork search

---
+++ src/artworks/ArtworkSearch.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import './ArtworkSearch.css';
+
+export default class ArtworkSearch extends Component {
+
+
+  
+  render() {
+    return (
+      <div className="ArtworkSearch">
+        <form>
+          <input/>
+          <button>Search</button>
+        </form>
+      </div>
+    );
+  }
+
+}
---
+++ src/artworks/Gallery.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Gallery.css';
+
+export default class Gallery extends Component {
+  
+  render() {
+    return (
+      <div className="Gallery">
+        
+      </div>
+    );
+  }
+
+}
