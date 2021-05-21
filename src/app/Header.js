aaf3a7296a41135f8f9becee51dab80051a9d3be
thankrandomness <vijaybchakilam@gmail.com>
2021-05-21 11:29:18 -0400

    Header name change

--- src/app/Header.js
+++ src/app/Header.js
@@ -3,18 +3,15 @@ import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
-
   render() {
     return (
-      <header className="Header">
-
-        <h1>React App</h1>
-        <NavLink to="/favorites" >Favorites</NavLink>
-        <NavLink to="/artworks" >Search</NavLink>
+      <header className='Header'>
+        <h1>Met Museum Artworks</h1>
+        <NavLink to='/favorites'>Favorites</NavLink>
+        <NavLink to='/artworks'>Search</NavLink>
       </header>
     );
   }
-
 }
 
 export default Header;
