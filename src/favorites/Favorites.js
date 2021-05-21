3e5f11963b220651f9bc3d76b02f6b7895580fdd
thankrandomness <vijaybchakilam@gmail.com>
2021-05-20 22:44:15 -0400

    favorites page

--- src/favorites/Favorites.js
+++ src/favorites/Favorites.js
@@ -1,29 +1,61 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import Gallery from '../artworks/Gallery';
+import {
+  addFavorite,
+  deleteFavorite,
+  getMyFavorites,
+} from '../utils/artworks-api';
 import './Favorites.css';
-import { getMyFavorites } from '../utils/artworks-api';
 
-export default class favorites extends Component {
+export default class Favorites extends Component {
   state = {
     favorites: [],
+  };
 
-  }
   async componentDidMount() {
+    this.setState({ favorites: await getMyFavorites() });
+  }
+
+  handleFavorite = async (artwork, isFavorite) => {
     try {
-      const artwork = await getMyFavorites();
-      this.setState({ favorites: artwork });
-    }
-    catch (err) {
+      const { favorites } = this.state;
+      if (isFavorite) {
+        const response = await addFavorite(artwork);
+        if (response.status !== 200) {
+          throw new Error(response.body);
+        }
+        const favorite = response.body;
+        // add artwork to favorites array
+        favorites.splice(1, 0, favorite);
+      } else {
+        // find the artwork id to delete
+        let index = 0;
+        for (let i = 0; i < favorites.length; i++) {
+          if (favorites[i].objectID === artwork.objectID) {
+            index = i;
+            break;
+          }
+        }
+        const favoriteId = favorites[index].id;
+        const response = await deleteFavorite(favoriteId);
+        if (response.status !== 200) {
+          throw new Error(response.body);
+        }
+        // delete the artwork from favorites array
+        favorites.splice(index, 1);
+      }
+      this.setState({ favorites: [...favorites] });
+    } catch (err) {
       console.log(err.message);
     }
-  }
+  };
+
   render() {
     const { favorites } = this.state;
     return (
-      <div className="favorites">
-        <Gallery favorites={favorites} />
+      <div className='Favorites'>
+        <Gallery artworks={favorites} onFavorited={this.handleFavorite} />
       </div>
     );
   }
-
 }
